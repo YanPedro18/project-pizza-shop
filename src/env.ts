a314fb6a1ddb0f82a5b2465ac840c46932b4f62c
Yan Pedro De Souza <68964255+YanPedro18@users.noreply.github.com>
2025-03-05 18:18:45 -0300

    feat: env de ambiente

---
+++ src/env.ts
@@ -0,0 +1,11 @@
+import {string, z} from 'zod'
+
+
+
+const envSchema = z.object({
+    MODE: z.enum(['production', 'development', 'test']),
+    VITE_API_URL: z.string(), //url() tirei.
+    VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
+})
+
+export const env = envSchema.parse(import.meta.env)
