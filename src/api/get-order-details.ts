b9c74b8af824fb460b8f2edb0292e620cec20e3f
Yan Pedro De Souza <68964255+YanPedro18@users.noreply.github.com>
2025-03-05 17:18:44 -0300

    feat: services e endpoints de requisição do front para o back-end

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,36 @@
+import { api } from "@/lib/axios"
+
+
+export interface OrderDetailsParams{
+    orderId: string;
+}
+
+export interface OrderDetailsResponse {
+        status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+        id: string;
+        createdAt: string;
+        totalInCents: number;
+        customer: {
+            name: string;
+            email: string;
+            phone: string | null;
+        };
+        orderItems: {
+            id: string;
+            priceInCents: number;
+            quantity: number;
+            product: {
+                name: string
+            }
+        }[];
+}
+
+export async function getOrderDetails({orderId}: OrderDetailsParams) {
+
+    const response = await api.get<OrderDetailsResponse>(`/orders/${orderId}`);
+
+
+    return response.data;
+
+}
+
